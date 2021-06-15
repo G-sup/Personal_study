c2d9da65aebbbcf8a180caca6610ebe7c44434eb
G-sup <jisup1994@gmail.com>
2021-06-15 16:08:57 +0900

    정리

--- padding_jeongli.py
+++ padding_jeongli.py
@@ -7,17 +7,24 @@ import torch
 from torch import nn
 
 # 기본데이터 생성
+
 t = np.array([[[[1, 2, 3], [4, 5, 6],[7,8,9]],[[1, 2, 3], [4, 5, 6],[7,8,9]]]])
 print(t)
+
+
 # ===============================================================================================
+
 # TENSORFLOW  (image 수, Height , Width, channel 수)
+
 inputs = tf.pad(t, [[0, 0], [0, 1], [0, 1], [0, 0]], mode="REFLECT")
 # [[image 수 앞장,뒷장],[top_pad, bottom_pad], [left_pad, right_pad],[channel 수 앞채널, 뒷채널]] 아니면 int(전체 pad)
 print(t.shape)
 print(inputs)
 
 # ===============================================================================================
+
 # PYTORCH  (image 수, channel 수, Height , Width)
+
 t1 = np.transpose(t, (0, 3, 1, 2))
 to = nn.ZeroPad2d((0,1,0,1))(torch.from_numpy(t1)) 
 # to = nn.ReflectionPad2d((0,1,0,1))(torch.from_numpy(t1)) 
@@ -28,7 +35,9 @@ print(to.shape)
 print(t2)
 
 # # ===============================================================================================
+
 # # KERAS  (image 수, Height , Width, channel 수)
+
 y = tf.keras.layers.ZeroPadding2D(padding=((0,1),(0,1)))(t)
 # (padding=((top_pad, bottom_pad), (left_pad, right_pad))) 아니면 (height_pad, width_pad).
 print(y)
