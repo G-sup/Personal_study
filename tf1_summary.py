a1e00e29d0a0becc615aac56323499bc08c6a44c
G-sup <jisup1994@gmail.com>
2021-06-16 10:52:41 +0900

    tf1.대 서머리

---
+++ tf1_summary.py
@@ -0,0 +1,9 @@
+import tensorflow as tf 
+
+def model_summary():
+    model_vars = tf.trainable_variables()
+    tf.contrib.slim.model_analyzer.analyze_vars(model_vars,print_info=True)
+
+model_summary()
+
+
